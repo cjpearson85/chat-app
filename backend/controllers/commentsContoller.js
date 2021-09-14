28041208a8d5d6be7fa2d4a22ee1b7da4169c2c5
Anthony <anthonymcgreal@hotmail.co.uk>
2021-09-14 11:35:40 +0100

    add routers and controllers

---
+++ backend/controllers/commentsContoller.js
@@ -0,0 +1,3 @@
+exports.getComments = (req, res, next) => {};
+exports.postComments = (req, res, next) => {};
+exports.patchComments = (req, res, next) => {};
---
+++ backend/routers/poi.Router.js
@@ -0,0 +1,13 @@
+const poiRouter = require('express').Router();
+const {
+  getPois,
+  patchPoi,
+  deletePoi,
+} = require('../controllers/poiController');
+
+poiRouter
+  .route('/')
+  .get(getPois) //get all pois for given route
+  .patch(patchPoi) //patch a poi
+  .delete(deletePoi); //delete a poi
+module.exports = poiRouter;
