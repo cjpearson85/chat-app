8266327cfaa93bb046e8685f28a2830b62aa9342
thick-hollins <alexnuttall@zoho.eu>
2021-09-17 16:26:59 +0100

    get pois by route

--- controllers/poiController.js
+++ controllers/poiController.js
@@ -1,4 +1,14 @@
-exports.getPois = (req, res, next) => {}
+const { selectPoisByRoute } = require('../models/poisModels')
+
+exports.getPois = (req, res, next) => {
+    console.log('in here')
+    const { route_id } = req.params
+    selectPoisByRoute(route_id)
+      .then((pois) => {
+        res.status(200).send({ pois })
+      })
+      .catch(next)
+}
 
 exports.patchPoi = (req, res, next) => {}
 
