f649ba2bee4d6d7ef7bcf325eebad242dcfa1e38
Anthony <anthonymcgreal@hotmail.co.uk>
2021-09-20 15:02:20 +0100

    final-merge

--- models/poisModels.js
+++ models/poisModels.js
@@ -1,12 +1,12 @@
-const Poi = require('../schemas/poi');
-const PoiLike = require('../schemas/poi-like');
-const db = require('../db/connection');
-const mongoose = require('mongoose');
+const Poi = require('../schemas/poi')
+const PoiLike = require('../schemas/poi-like')
+const db = require('../db/connection')
+const mongoose = require('mongoose')
 
 exports.selectPoisByRoute = async (route_id) => {
-  const result = await Poi.find({ route_id: `${route_id}` });
-  return result;
-};
+  const result = await Poi.find({ route_id: `${route_id}` })
+  return result
+}
 
 exports.insertPoi = async (
   user_id,
@@ -16,7 +16,7 @@ exports.insertPoi = async (
   { route_id }
 ) => {
   if (!coords || !user_id || !route_id) {
-    return Promise.reject({ status: 400, msg: 'Bad request' });
+    return Promise.reject({ status: 400, msg: 'Bad request' })
   }
   const poi = new Poi({
     user_id,
@@ -24,60 +24,60 @@ exports.insertPoi = async (
     coords,
     photo: imageLink || null,
     narration: narration || null,
-  });
-  const result = await poi.save();
-  return result;
-};
+  })
+  const result = await poi.save()
+  return result
+}
 
 exports.updatePoi = async ({ photo, narration, likes, user }, { poi_id }) => {
   if (!photo && !narration && !likes) {
     return Promise.reject({
       status: 400,
       msg: 'Bad request - missing field(s)',
-    });
+    })
   }
   if (likes && !user) {
     return Promise.reject({
       status: 400,
       msg: 'Bad request - missing field(s)',
-    });
+    })
   }
   if (likes) {
-    const existingLike = await PoiLike.findOne({ user_id: user, poi_id });
+    const existingLike = await PoiLike.findOne({ user_id: user, poi_id })
     if (existingLike) {
       if (likes === 1) {
         return Promise.reject({
           status: 400,
           msg: 'Bad request - duplicate like',
-        });
+        })
       }
       if (likes === -1) {
-        await PoiLike.deleteOne({ _id: existingLike._id });
+        await PoiLike.deleteOne({ _id: existingLike._id })
       }
     } else {
       if (likes === -1) {
         return Promise.reject({
           status: 400,
           msg: 'Bad request - like not found',
-        });
+        })
       }
       const poiLike = new PoiLike({
         user_id: user,
         poi_id,
-      });
-      await poiLike.save();
+      })
+      await poiLike.save()
     }
   }
 
-  const poiLikes = await Poi.findById(poi_id).select('likes');
+  const poiLikes = await Poi.findById(poi_id).select('likes')
   if (poiLikes.likes === 0 && likes === -1) {
     return Promise.reject({
       status: 400,
       msg: 'Bad request - likes are already zero',
-    });
+    })
   }
-  if (!likes) likes = poiLikes.likes;
-  else likes = poiLikes.likes + likes;
+  if (!likes) likes = poiLikes.likes
+  else likes = poiLikes.likes + likes
   const result = await Poi.findByIdAndUpdate(
     poi_id,
     {
@@ -86,10 +86,10 @@ exports.updatePoi = async ({ photo, narration, likes, user }, { poi_id }) => {
       likes,
     },
     { new: true }
-  );
-  return result;
-};
+  )
+  return result
+}
 
 exports.removePoi = async ({ poi_id }) => {
-  return Poi.findByIdAndDelete(poi_id);
-};
+  return Poi.findByIdAndDelete(poi_id)
+}
