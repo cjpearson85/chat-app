1a16c5c2e8f285ea2db18d908717bca9845f880f
Anthony <anthonymcgreal@hotmail.co.uk>
2021-09-21 10:59:50 +0100

    refactor POI controller and model

--- controllers/poiController.js
+++ controllers/poiController.js
@@ -3,55 +3,49 @@ const {
   insertPoi,
   updatePoi,
   removePoi,
-  generateUrl
-} = require('../models/poisModels')
-const { uploadImage } = require('../models/s3')
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+  generateUrl,
+} = require('../models/poisModels');
+const { uploadImage } = require('../models/s3');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 exports.getPois = (req, res, next) => {
-  const { route_id } = req.params
+  const { route_id } = req.params;
   selectPoisByRoute(route_id)
     .then((pois) => {
-      res.status(200).send({ pois })
+      res.status(200).send({ pois });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
 exports.postPoi = async (req, res, next) => {
-  let imageLink
-  // if (req.body.photo) {
-  //   imageLink = await uploadImage(req.body.photo)
-  // }
-  const { user_id, narration, coords } = req.body
-  await insertPoi(user_id, imageLink, narration, coords, req.params)
-
+  const { user_id, photo, narration, coords } = req.body;
+  await insertPoi(user_id, photo, narration, coords, req.params)
     .then((poi) => {
-      res.status(201).send({ poi })
+      res.status(201).send({ poi });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
 exports.patchPoi = (req, res, next) => {
   updatePoi(req.body, req.params)
-    .then(poi => {
-      res.status(200).send({ poi })
+    .then((poi) => {
+      res.status(200).send({ poi });
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
 exports.deletePoi = (req, res, next) => {
   removePoi(req.params)
     .then(() => {
-      res.status(204).send()
+      res.status(204).send();
     })
-    .catch(next)
-}
-
+    .catch(next);
+};
 
 exports.getUrl = (req, res, next) => {
   generateUrl()
     .then((uri) => {
-      res.status(200).send({uri})
+      res.status(200).send({ uri });
     })
-    .catch(next)
-}
+    .catch(next);
+};
--- models/poisModels.js
+++ models/poisModels.js
@@ -1,85 +1,83 @@
-const Poi = require('../schemas/poi')
-const PoiLike = require('../schemas/poi-like')
-const db = require('../db/connection')
-const mongoose = require('mongoose')
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner")
-const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3")
+const Poi = require('../schemas/poi');
+const PoiLike = require('../schemas/poi-like');
+const db = require('../db/connection');
+const mongoose = require('mongoose');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require('@aws-sdk/client-s3');
 
 exports.selectPoisByRoute = async (route_id) => {
-  const result = await Poi.find({ route_id: `${route_id}` })
-  return result
-}
+  const result = await Poi.find({ route_id: `${route_id}` });
+  return result;
+};
 
-exports.insertPoi = async (
-  user_id,
-  imageLink,
-  narration,
-  coords,
-  { route_id }
-) => {
+exports.insertPoi = async (user_id, photo, narration, coords, { route_id }) => {
   if (!coords || !user_id || !route_id) {
-    return Promise.reject({ status: 400, msg: 'Bad request' })
+    return Promise.reject({ status: 400, msg: 'Bad request' });
   }
   const poi = new Poi({
     user_id,
     route_id,
     coords,
-    photo: imageLink || null,
+    photo: photo || null,
     narration: narration || null,
-  })
-  const result = await poi.save()
-  return result
-}
+  });
+  const result = await poi.save();
+  return result;
+};
 
 exports.updatePoi = async ({ photo, narration, likes, user }, { poi_id }) => {
   if (!photo && !narration && !likes) {
     return Promise.reject({
       status: 400,
       msg: 'Bad request - missing field(s)',
-    })
+    });
   }
   if (likes && !user) {
     return Promise.reject({
       status: 400,
       msg: 'Bad request - missing field(s)',
-    })
+    });
   }
   if (likes) {
-    const existingLike = await PoiLike.findOne({ user_id: user, poi_id })
+    const existingLike = await PoiLike.findOne({ user_id: user, poi_id });
     if (existingLike) {
       if (likes === 1) {
         return Promise.reject({
           status: 400,
           msg: 'Bad request - duplicate like',
-        })
+        });
       }
       if (likes === -1) {
-        await PoiLike.deleteOne({ _id: existingLike._id })
+        await PoiLike.deleteOne({ _id: existingLike._id });
       }
     } else {
       if (likes === -1) {
         return Promise.reject({
           status: 400,
           msg: 'Bad request - like not found',
-        })
+        });
       }
       const poiLike = new PoiLike({
         user_id: user,
         poi_id,
-      })
-      await poiLike.save()
+      });
+      await poiLike.save();
     }
   }
 
-  const poiLikes = await Poi.findById(poi_id).select('likes')
+  const poiLikes = await Poi.findById(poi_id).select('likes');
   if (poiLikes.likes === 0 && likes === -1) {
     return Promise.reject({
       status: 400,
       msg: 'Bad request - likes are already zero',
-    })
+    });
   }
-  if (!likes) likes = poiLikes.likes
-  else likes = poiLikes.likes + likes
+  if (!likes) likes = poiLikes.likes;
+  else likes = poiLikes.likes + likes;
   const result = await Poi.findByIdAndUpdate(
     poi_id,
     {
@@ -88,22 +86,22 @@ exports.updatePoi = async ({ photo, narration, likes, user }, { poi_id }) => {
       likes,
     },
     { new: true }
-  )
-  return result
-}
+  );
+  return result;
+};
 
 exports.removePoi = async ({ poi_id }) => {
-  return Poi.findByIdAndDelete(poi_id)
-}
+  return Poi.findByIdAndDelete(poi_id);
+};
 
 exports.generateUrl = async () => {
-  const client = new S3Client({ region: process.env.AWSREGION })
+  const client = new S3Client({ region: process.env.AWSREGION });
   const command = new PutObjectCommand({
-    Bucket: process.env.AWSBUCKETNAME, 
+    Bucket: process.env.AWSBUCKETNAME,
     Key: 'test.jpg',
-    'content-type': 'photo/jpg'
+    'content-type': 'photo/jpg',
     // ACL:'public-read'
-  })
-  const url = await getSignedUrl(client, command, { expiresIn: 3600 })
-  return url
-}
+  });
+  const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+  return url;
+};
